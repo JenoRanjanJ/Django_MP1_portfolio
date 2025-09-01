9f021eabe1f9b1abc7d0b4cc68c28faea1968329
JenoRanjanJ <jenoranjan.1110@gmail.com>
2025-09-01 17:23:01 +0530

    Update settings.py

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -26,9 +26,7 @@ SECRET_KEY = 'django-insecure-(2-+2+9h+ji@i9m4@fu@lde-2b)9oovb10!75p2ffa#3gvv3@q
 DEBUG = True
 
 ALLOWED_HOSTS = [
-"localhost",
-    "127.0.0.1",
-    "django-mp1-portfolio-1.onrender.com",  
+'*'  
 ]
 
 
